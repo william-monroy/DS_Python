b812d06581f022fe7b2309c37ebae02b69ef0b03
William Monroy <A00829796@itesm.mx>
2021-01-19 12:16:50 -0500

    Asignacion Multiple

--- Asignacion Multiple/main.py
+++ Asignacion Multiple/main.py
@@ -2,4 +2,21 @@
 
 print("En el Ñágara encontré un Ñandú")
 
+# Variables independientes
+a, b, c = 'string', 123, 3.1416
+print(type(a), a)
+print(type(b), b)
+print(type(c), c)
+
+# A partir de una tupla
+mi_tupla = ("hola mundo", 2011)
+texto, year = mi_tupla
+print(type(texto), texto)
+print(type(year), year)
+
+# A partir de una lista
+mi_lista = ['Peru', 'Arequipa']
+pais, provincia = mi_lista
+print(type(pais), pais)
+print(type(provincia), provincia)
 
